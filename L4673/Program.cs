6405f223f4bb37db8c5d3c9258e6b5c9e887d86c
t-j <t-j@terence-john.co.uk>
2022-07-27 16:13:26 +0100

    about a third of answer acquired

--- L4673/Program.cs
+++ L4673/Program.cs
@@ -7,54 +7,199 @@ namespace L4673
     {
         static void Main(string[] args)
         {
+            int[] prime = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53 };
+
+            int[] all_square2 = { 16, 25, 36, 49, 64, 81 };
+            int[] all_square3 = { 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 900, 961 };
+
+            List<int> cubes = new List<int> { 125, 216, 343, 512, 729 };
+
             {
-                Console.WriteLine("-----------");
-                int T = 165;
+                // -- here are all the possible value for 'H'
 
-                int[] V = { 18, 27, 54, 81 };
+                List<int> poss_H = new List<int> { 4181, 6765 };
 
-                int[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53 };
-                int[] squares = { 1296, 4761, 5476, 6561, 7056, 9216 };
+                //-- Have a look at b, c & Y
 
-                foreach (int v in V)
-                    foreach (int p in primes)
+                List<int> poss_b = new List<int>();
+                List<int> poss_c = new List<int>();
+                List<int> poss_Y = new List<int>();
+
+                foreach (int c_ in cubes)
+                {
+                    if (IsValid(c_))
                     {
-                        int U = v * p;
-                        if (U > 100 && U < 1000)
+                        int Y_ = c_ + c_;
+                        if (Y_ >= 1000 && IsValid(Y_))
                         {
-                            int W = (U - T) * (U - T);
-                            if (W > 1000 && W < 10000 && valid(W))
-                                Console.WriteLine($"V={v} U={U} W={W} (prime={p})");
+                            poss_c.Add(c_);
+                            poss_Y.Add(Y_);
                         }
+                        else
+                            poss_b.Add(c_);
                     }
-            }
-            {
-                Console.WriteLine("-----------");
-                int c = 729;
-                int B = 21;
-
-                int[] bList = { 125, 216, 343 };
-                foreach (int b in bList)
-                {
-                    int R = c - B - b;
-                    if (valid(R))
-                        Console.WriteLine($"b={b} R={R}");
                 }
 
-            }
-            {
-                Console.WriteLine("-----------");
-                int T = 165;
-                int n = 2;
-                int E = n * n * T;
-                while (E < 100000)
+                int c = 0, Y = 0;
+                if (poss_c.Count == 1)
                 {
-                    if (E > 10000 && valid(E))
-                        Console.WriteLine($"E={E} (n={n})");
-                    n++;
-                    E = n * n * T;
+                    c = poss_c[0];
+                    Y = poss_Y[0];
+                    poss_b.Remove(c);
+                }
+
+                // -- Try for T, U, V and W
+
+                List<int> poss_T = new List<int>();
+                List<int> poss_U = new List<int>();
+                List<int> poss_V = new List<int>();
+                List<int> poss_W = new List<int>();
+
+                int h_value = 0;
+                foreach (int H_ in poss_H)
+                    foreach (int T_ in Factorize(H_, 3))
+                        foreach (int V_ in Factorize(Y, 2))
+                            foreach (int prm in prime)
+                            {
+                                int U_ = V_ * prm;
+                                if (U_ >= 100 && U_ < 1000)
+                                {
+                                    int W_ = (U_ - T_) * (U_ - T_);
+                                    if (W_ >= 1000 && W_ < 10000 && IsValid(W_))
+                                    {
+                                        h_value = H_;
+                                        poss_T.Add(T_);
+                                        poss_U.Add(U_);
+                                        poss_V.Add(V_);
+                                        poss_W.Add(W_);
+
+                                    }
+                                }
+                            }
+                int H = 0, T = 0, U = 0, V = 0, W = 0;
+                if (poss_T.Count == 1)
+                {
+                    H = h_value;
+                    T = poss_T[0];
+                    U = poss_U[0];
+                    V = poss_V[0];
+                    W = poss_W[0];
+                }
+
+                // -- we can also just calc C now
+                int C = H + c;
+
+                // -- Have a quick look at E
+                List<int> poss_E = new List<int>();
+                for (int ind = 10; T * ind * ind < 100000; ind++)
+                {
+                    int E_ = T * ind * ind;
+                    if (E_ >= 10000 && IsValid(E_))
+                        poss_E.Add(E_);
+                }
+
+                int E = 0;
+                if (poss_E.Count == 1)
+                {
+                    E = poss_E[0];
+                }
+
+                // -- how are we doing for L and b
+                List<int> poss_L = new List<int>();
+                foreach (int b_ in poss_b)
+                {
+                    int L_ = E + b_;
+                    if (IsValid(L_))
+                    {
+                        poss_L.Add(L_);
+                    }
+                }
+                int L = 0;
+                int b = 0;
+                if (poss_L.Count == 1)
+                {
+                    L = poss_L[0];
+                    b = L - E;
+                    poss_b = new List<int> { b };
+                }
+
+                // -- let's try f and M
+                List<int> poss_f = new List<int>();
+                List<int> poss_M = new List<int>();
+
+                for (int ind = 1; V + ind * ind < 100; ind++)
+                {
+                    int f_ = V + ind * ind;
+                    int M_ = V * f_;
+                    if (IsValid(f_) && IsValid(M_))
+                    {
+                        poss_f.Add(f_);
+                        poss_M.Add(M_);
+                    }
+                }
+                int f = 0, M = 0;
+                if (poss_M.Count == 1)
+                {
+                    M = poss_M[0];
+                    f = poss_f[0];
+                }
+
+                // -- we now know K and a
+                int K = V + f;
+                int a = T * f;
+
+                // -- try for d
+                List<int> poss_d = new List<int>();
+
+                for (int ind = 2; K * ind * ind < 1000; ind++)
+                {
+                    int d_ = K * ind * ind;
+                    if (d_ >= 100 && IsValid(d_))
+                    {
+                        poss_d.Add(d_);
+                    }
+                }
+                int d = 0;
+                if (poss_d.Count == 1)
+                    d = poss_d[0];
+
+
+
+
+                Console.WriteLine("------");
+                Console.WriteLine($"C={C,5}");
+                Console.WriteLine($"E={E,5}");
+                Console.WriteLine($"H={H,5}");
+                Console.WriteLine($"K={K,5}");
+                Console.WriteLine($"L={L,5}");
+                Console.WriteLine($"M={M,5}");
+                Console.WriteLine($"T={T,5}");
+                Console.WriteLine($"U={U,5}");
+                Console.WriteLine($"V={V,5}");
+                Console.WriteLine($"W={W,5}");
+                Console.WriteLine($"Y={Y,5}");
+                Console.WriteLine($"a={a,5}");
+                Console.WriteLine($"b={b,5}");
+                Console.WriteLine($"c={c,5}");
+                Console.WriteLine($"d={d,5}");
+                Console.WriteLine($"f={f,5}");
+                Console.WriteLine("------");
+
+
+
+
+                //// -------------------------------------------------------------
+                Console.WriteLine();
+                Console.WriteLine("----------- These are speculative");
+                //int B = 21;
+                for (int B_ = 10; B_ < 100; B_++)
+                {
+                    int R = c - B_ - b;
+                    if (IsValid(R) && R.ToString().Length == 3)
+                        Console.WriteLine($"b={b} R={R} B={B_}");
                 }
             }
+
 
             {
                 Console.WriteLine("-----------");
@@ -63,19 +208,19 @@ namespace L4673
                 int e = V + n * n;
                 while (e < 100)
                 {
-                    if (valid(e) && e != 34)
+                    if (IsValid(e) && e != 34)
                     {
                         int Q = 21 + e - 41;
-                        if (Q > 9 && valid(Q) && Q != 34)
+                        if (Q > 9 && IsValid(Q) && Q != 34)
                         {
                             int D = 612 + Q - 21;
-                            if (valid(D))
+                            if (IsValid(D))
                             {
                                 int p = 2;
                                 int N = Q * p * p;
                                 while (N < 10000)
                                 {
-                                    if (N > 1000 && valid(N))
+                                    if (N > 1000 && IsValid(N))
                                         Console.WriteLine($"e={e} Q={Q} D={D} N={N}");
                                     p++;
                                     N = Q * p * p;
@@ -87,23 +232,22 @@ namespace L4673
                     e = V + n * n;
                 }
             }
-            {
-                Console.WriteLine("-----------");
-                int[] cubes = { 125, 216, 343 };
-                int E = 72765;
 
-                foreach (int b in cubes)
-                {
-                    int L = E + b;
-                    if (valid(L))
-                    {
-                        Console.WriteLine($"b={b} L={L}");
-                    }
-                }
-            }
 
         }
-        private static bool valid(int n)
+
+        private static List<int> Factorize(int y, int v)
+        {
+            List<int> retval = new List<int>();
+            for (int i = 1; i <= y; i++)
+            {
+                if (i.ToString().Length == v && y % i == 0 && IsValid(i) && !retval.Contains(i))
+                    retval.Add(i);
+            }
+            return (retval);
+        }
+
+        private static bool IsValid(int n)
         {
             string str = n.ToString();
             bool nextIsOdd = "2468".Contains(str[0]);
